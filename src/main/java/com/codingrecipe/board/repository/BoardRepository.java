7962f3a13055df5125df6583a6d618fe43add29a
JAEHYEOK <tmzkdlqpffl@gmail.com>
2024-01-26 15:05:59 +0900

    게시판 프로젝트_02_게시판 글쓰기(01)

---
+++ src/main/java/com/codingrecipe/board/repository/BoardRepository.java
@@ -0,0 +1,15 @@
+package com.codingrecipe.board.repository;
+
+import com.codingrecipe.board.dto.BoardDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class BoardRepository {
+
+
+
+    public int save(BoardDTO boardDTO) {
+    }
+}
