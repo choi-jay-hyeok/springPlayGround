0a19692b74c243d9ec21744e757ab6db16a8576c
JAEHYEOK <tmzkdlqpffl@gmail.com>
2024-01-31 18:09:20 +0900

    스프링부트 회원_08_회원정보 상세조회

---
+++ src/README.md
@@ -0,0 +1,17 @@
+## Spring Boot + MySQL + Spring Data JPA를 활용한 회원 프로젝트
+
+### 개발환경
+- IntelliJ IDEA Community Edition
+- amazon corretto open jdk 17
+- mysql 8
+- Spring Data JPA
+- Thymeleaf
+- Tomcat 9
+
+### 주요 기능
+- 회원가입
+- 로그인
+- 회원목록
+- 회원정보 수정
+- 회원 삭제
+- 로그아웃
--- src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -6,10 +6,7 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -63,5 +60,12 @@ public class MemberController {
         //특정 html로 가져갈 데이터가 있다면 Model 사용
         model.addAttribute("memberList", memberDTOList);
         return "list";
+    }
+
+    @GetMapping("/member/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        MemberDTO memberDTO = memberService.findById(id);
+        model.addAttribute("member", memberDTO);
+        return "detail";
     }
 }
