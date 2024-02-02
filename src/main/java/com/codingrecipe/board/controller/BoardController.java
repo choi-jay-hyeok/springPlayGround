0dc9b0b4d6b1ed032a67103a95cfbe1bf7399a0f
JAEHYEOK <tmzkdlqpffl@gmail.com>
2024-02-02 15:26:44 +0900

    스프링부트 게시판_06_게시글 수정

--- src/main/java/com/codingrecipe/board/controller/BoardController.java
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -45,5 +45,20 @@ public class BoardController {
         model.addAttribute("board", boardDTO);
         return "detail";
     }
+
+    @GetMapping("/update/{id}")
+    public String updateForm(@PathVariable Long id, Model model) {
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("boardUpdate", boardDTO);
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute BoardDTO boardDTO, Model model) {
+        BoardDTO board = boardService.update(boardDTO);
+        model.addAttribute("board", board);
+        return "detail";
+//        return "redirect:/board/" + boardDTO.getId(); //현재 코드에서는 redirect로 인해 수정을 해도 조회수가 1 올라감
+    }
 }
 
